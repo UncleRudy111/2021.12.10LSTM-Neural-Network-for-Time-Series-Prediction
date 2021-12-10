45d811e4e38b7b038fec1e01d9413db115254580
UncleRudy111 <1650141458@qq.com>
2021-12-10 18:36:51 +0800

    update

--- run.py
+++ run.py
@@ -33,59 +33,58 @@ def plot_results_multiple(predicted_data, true_data, prediction_len):
     plt.show()
 
 
-def main():
-    configs = json.load(open('config.json', 'r'))
-    if not os.path.exists(configs['model']['save_dir']): os.makedirs(configs['model']['save_dir'])
 
-    data = DataLoader(
-        os.path.join('data', configs['data']['filename']),
-        configs['data']['train_test_split'],
-        configs['data']['columns']
-    )
+configs = json.load(open('config.json', 'r'))
+if not os.path.exists(configs['model']['save_dir']): os.makedirs(configs['model']['save_dir'])
 
-    model = Model()
-    model.build_model(configs)
-    x, y = data.get_train_data(
-        seq_len=configs['data']['sequence_length'],
-        normalise=configs['data']['normalise']
-    )
+data = DataLoader(
+    os.path.join('data', configs['data']['filename']),
+    configs['data']['train_test_split'],
+    configs['data']['columns']
+)
 
-    '''
-	# in-memory training
-	model.train(
-		x,
-		y,
-		epochs = configs['training']['epochs'],
-		batch_size = configs['training']['batch_size'],
-		save_dir = configs['model']['save_dir']
-	)
-	'''
-    # out-of memory generative training
-    steps_per_epoch = math.ceil((data.len_train - configs['data']['sequence_length']) / configs['training']['batch_size'])
-    model.train_generator(
-        data_gen=data.generate_train_batch(
-            seq_len=configs['data']['sequence_length'],
-            batch_size=configs['training']['batch_size'],
-            normalise=configs['data']['normalise']
-        ),
-        epochs=configs['training']['epochs'],
-        batch_size=configs['training']['batch_size'],
-        steps_per_epoch=steps_per_epoch,
-        save_dir=configs['model']['save_dir']
-    )
+model = Model()
+model.build_model(configs)
+x, y = data.get_train_data(
+    seq_len=configs['data']['sequence_length'],
+    normalise=configs['data']['normalise']
+)
 
-    x_test, y_test = data.get_test_data(
-        seq_len=configs['data']['sequence_length'],
-        normalise=configs['data']['normalise']
-    )
+# in-memory training
+model.train(
+    x,
+    y,
+    epochs = configs['training']['epochs'],
+    batch_size = configs['training']['batch_size'],
+    save_dir = configs['model']['save_dir']
+)
 
-    predictions = model.predict_sequences_multiple(x_test, configs['data']['sequence_length'], configs['data']['sequence_length'])
-    # predictions = model.predict_sequence_full(x_test, configs['data']['sequence_length'])
-    # predictions = model.predict_point_by_point(x_test)
-
-    plot_results_multiple(predictions, y_test, configs['data']['sequence_length'])
-    # plot_results(predictions, y_test)
+'''
+# out-of memory generative training
+steps_per_epoch = math.ceil((data.len_train - configs['data']['sequence_length']) / configs['training']['batch_size'])
+data_gen = data.generate_train_batch(
+    seq_len=configs['data']['sequence_length'],
+    batch_size=configs['training']['batch_size'],
+    normalise=configs['data']['normalise']
+)
+model.train_generator(
+    data_gen=data_gen,
+    epochs=configs['training']['epochs'],
+    batch_size=configs['training']['batch_size'],
+    steps_per_epoch=steps_per_epoch,
+    save_dir=configs['model']['save_dir']
+)
+'''
 
 
-if __name__ == '__main__':
-    main()
+x_test, y_test = data.get_test_data(
+    seq_len=configs['data']['sequence_length'],
+    normalise=configs['data']['normalise']
+)
+
+# predictions = model.predict_sequences_multiple(x_test, configs['data']['sequence_length'], configs['data']['sequence_length'])
+# predictions = model.predict_sequence_full(x_test, configs['data']['sequence_length'])
+predictions = model.predict_point_by_point(x_test)
+
+# plot_results_multiple(predictions, y_test, configs['data']['sequence_length'])
+plot_results(predictions, y_test)
